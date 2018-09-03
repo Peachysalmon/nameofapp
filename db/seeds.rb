14f4fb24e26777387f65a19664332533603d6e64
Peachysalmon <info@jonathan-meier.com>
2018-09-03 16:16:46 +0200

    4.2 Added/migrated properties, styled index product page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Bike Six", description: "Same as Bike 5 with some minor changes", image_url:"race_bike5.jpg")
