b5224f5a6680fe8cae631b6092d1c4dba2e7762b
Peachysalmon <info@jonathan-meier.com>
2018-10-16 16:39:35 +0200

    5.3 Added Mails plugin

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,10 +1,16 @@
 class SimplePagesController < ApplicationController
   def index
   end
-end
-
-class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(5)
+  end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'info@jonathan-meier.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
   end
 end
