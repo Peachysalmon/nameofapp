66c2e3ec13e521f820a769f745cc2fd12049a68f
Peachysalmon <info@jonathan-meier.com>
2018-10-22 10:54:46 +0200

    5.7 Added Reviews(Comments) to product page

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,14 @@ class Product < ApplicationRecord
       Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
+  has_many :comment
+  def highest_rating_comment
+    comment.rating_desc.first
+  end
+  def lowest_rating_comment
+    comment.rating_asc.first
+  end
+  def newest_rating_comment
+    comment.created_at_desc.first
+  end
 end
