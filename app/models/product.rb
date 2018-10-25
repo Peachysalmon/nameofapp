c6e71ca83e4000b7ed51c1e9ec584616e688e8ed
Peachysalmon <info@jonathan-meier.com>
2018-10-25 10:53:04 +0200

    5.7(2) Added Comments (again)

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,11 @@ class Product < ApplicationRecord
       Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
+  has_many :comments
+  def highest_rating_comment
+    comments.rating_desc.first.body
+  end
+  def lowest_rating_comment
+    comments.rating_asc.first.body
+  end
 end
