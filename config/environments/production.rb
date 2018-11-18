b32a186efe25aac70663a12d6a663bceb3fdb5f1
Peachysalmon <info@jonathan-meier.com>
2018-11-18 21:52:39 +0100

    6.13 ActionCable part_1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'jons-bike-berlin-demo.herokuapp.com' }
 
+  config.web_socket_server_url = "wss://jons-bike-berlin-demo.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://jons-bike-berlin-demo.herokuapp.com', 'http://jons-bike-berlin-demo.herokuapp.com']
+
 end
